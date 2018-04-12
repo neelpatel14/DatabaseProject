f21381f942152c3dc913d781969cb371bdfed38a
Neel Patel <np2ch@virginia.edu>
2018-04-12 02:36:53 -0400

    switching to ajax

--- data/BetSelect.php
+++ data/BetSelect.php
@@ -1,20 +1,34 @@
 <?php
- 	require "dbutil.php";
- 	$db = DbUtil::loginConnection();
+ require_once('./library.php');
+ $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+ // Check connection
+ if (mysqli_connect_errno()) {
+ echo("Can't connect to MySQL Server. Error code: " .
+mysqli_connect_error());
+ return null;
+ }
+ // Form the SQL query (a SELECT query)
+ $sql="SELECT * FROM PJ_Bets";
+ $result = mysqli_query($con,$sql);
+ // Print the data from the table row by row
+ echo "<table border=1><tr><th>betid</th><th>Amount</th><th>Odds</th><th>Outcome</th><th>Type</th></tr>\n";
+ while($row = mysqli_fetch_array($result)) {
+   echo "<tr>";
+   echo "<td>" . $row['betid'] . "</td>";
+   echo "<td>" . $row['Amount'] . "</td>";
+   echo "<td>" . $row['Odds'] . "</td>";
+   echo "<td>" . $row['Outcome'] . "</td>";
+   echo "<td>" . $row['Type'] . "</td>";
+   echo "</tr>";
+ // echo $row['betid'];
+ // echo " " . $row['Amount'];
+ // echo " " . $row['Odds'];
+ // echo " " . $row['Outcome'];
+ // echo " " . $row['Type'];
+ // echo "<br>";
+ // echo "<tr><td>$row['betid']</td><td>$row['Amount']</td><td>$row['Odds']</td><td>$row['Outcome']</td><td>$row['Type']</td></tr>";
+}
+echo "</table>";
 
- 	$stmt = $db->stmt_init();
-
- 	if($stmt->prepare("select * from PJ_Bets") or die(mysqli_error($db))) {
- 		$stmt->execute();
- 		$stmt->bind_result($betid, $Amount, $Odds, $Outcome, $Type);
- 		echo "<table border=1><th>betid</th><th>Amount</th><th>Odds</th><th>Outcome</th><th>Type</th>\n";
- 		while($stmt->fetch()) {
- 			echo "<tr><td>$betid</td><td>$Amount</td><td>$Odds</td><td>$Outcome</td><td>$Type</td></tr>";
- 		}
- 		echo "</table>";
-
- 		$stmt->close();
- 	}
-
- 	$db->close();
-?>
+ mysqli_close($con);
+ ?>
---
+++ data/library.php
@@ -0,0 +1,6 @@
+<?php
+  $SERVER = 'stardock.cs.virginia.edu';
+  $USERNAME = 'CS4750mih7gf';
+  $PASSWORD = '-----------';
+  $DATABASE = 'CS4750mih7gf';
+ ?>
